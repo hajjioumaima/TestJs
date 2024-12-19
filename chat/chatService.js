1c351fda7785987603c9229ca61c29a4bfb855b9
hajjioumaima <Oumaima.Hajji@esprit.tn>
2024-12-19 17:29:18 +0000

    Ajout des modifications pour la branche javaScript

---
+++ chat/chatService.js
@@ -0,0 +1,75 @@
+var Chat = require('./chatModel');
+var Ordinateur = require('../ordinateur/ordinateurModel');
+var socketIo = require('socket.io');
+
+function socketIO(server) {
+    const io = socketIo(server);
+    io.on('connection', (socket) => {
+        console.log("user connected !");
+        socket.broadcast.emit("msg", "A new user is connected !");
+
+        socket.on('chatMessage', (msg) => {
+            io.emit('msg', msg);
+        });
+
+        socket.on('searchOrdinateur', async (category) => {
+            try {
+                const ordinateurs = await Ordinateur.find({ category: category });
+                socket.emit('ordinateurResults', ordinateurs);
+            } catch (err) {
+                console.error(err);
+            }
+        });
+    });
+    return io;
+}
+
+function chatView(req, res, next) {
+    res.render('chat');
+}
+
+async function list(req, res, next) {
+    await Chat.find()
+        .then((data, err) => {
+            if (err) {
+                res.status(500).json(err);
+            }
+            res.status(200).json(data);
+        });
+}
+
+const create = async (req, res, next) => {
+    const { msg } = req.body;
+    console.log(req.body.msg);
+    await new Chat({
+        msg: msg,
+        date: new Date()
+    }).save()
+        .then((data, err) => {
+            if (err) {
+                res.status(500).json(err);
+            }
+            console.log(data);
+        });
+
+    res.json('Chat added ! msg : ' + msg + ' date : ' + new Date());
+}
+
+const update = async (req, res, next) => {
+    await Chat.findByIdAndUpdate(req.params.id, req.body)
+        .then((data, err) => {
+            res.json(data);
+        });
+}
+
+async function deleteU(req, res, next) {
+    await Chat.findByIdAndDelete(req.params.id)
+        .then((data, err) => {
+            if (err) {
+                res.status(500).json(err);
+            }
+            res.status(200).json(data);
+        });
+}
+
+module.exports = { socketIO, chatView, create, list, update, deleteU };
---
+++ ordinateur/controller.js
@@ -0,0 +1,13 @@
+// ordinateur/controller.js
+var express = require('express');
+var router = express.Router();
+const { list, create, update, deleteU, searchByPrice , ordinateurView } = require('./ordinateurService');
+
+router.get('/list', list);
+router.post('/create', create);
+router.put('/update/:id', update);
+router.delete('/delete/:id', deleteU);
+router.get('/search', searchByPrice);
+router.get('/searchPage', ordinateurView);
+
+module.exports = router;
